c94a66c5aa225b24b7a371eb47ef07ad818798af
Ranon Emerick <ranonemerick@gmail.com>
2024-03-07 11:48:01 -0300

    configuracao do kafkatemplate e replicacao da classes para outros microservicos

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/config/kafka/KafkaConfig.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/config/kafka/KafkaConfig.java
@@ -1,4 +1,4 @@
-package br.com.microservices.orchestrated.orderservice.config.kafka;
+package br.com.microservices.orchestrated.paymentservice.config.kafka;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
