fc98d7b57b21db7b040f9e448ab29a02f47426fb
Ranon Campos <ranonemerickcampos@gmail.com>
2026-07-13 20:38:07 -0300

    feat(order): implement saga ending notification handler and consumer
    - Update `EventConsumer` to listen to the `notify-ending` Kafka topic
    - Add `notifyEnding` method in `EventService` to persist the final saga state
    - Implement JSON deserialization and logging for the final event outcome

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -3,13 +3,22 @@ package br.com.microservices.orchestrated.orderservice.core.service;
 import br.com.microservices.orchestrated.orderservice.core.document.Event;
 import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class EventService {
 
     private final EventRepository eventRepository;
+
+    public void notifyEnding(Event event) {
+        event.setOrderId(event.getId());
+        event.setCreatedAt(event.getCreatedAt());
+        save(event);
+        log.info("Order {} with saga notified! TransactionId {}", event.getOrderId(), event.getTransactionId());
+    }
 
     public Event save(Event event) {
         return eventRepository.save(event);
