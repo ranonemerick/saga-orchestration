8de489b6c2865c6a8a805b34a63c71cf6b98230d
Ranon Campos <ranonemerickcampos@gmail.com>
2026-07-13 13:48:44 -0300

    feat(order): set up MongoDB data persistence and request DTOs
    - Add `@Document` and `@Id` annotations to `Order` and `Event` documents
    - Create `OrderRepository` and `EventRepository` interfaces extending MongoRepository
    - Add `OrderRequest` and `EventFilters` DTOs to handle incoming payload data

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
@@ -4,16 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import nonapi.io.github.classgraph.json.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@Document(collection = "event")
 public class Event {
 
+    @Id
     private String id;
     private String transactionId;
     private String orderId;
--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
@@ -5,16 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import nonapi.io.github.classgraph.json.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@Document(collection = "order")
 public class Order {
 
+    @Id
     private String id;
     private List<OrderProducts> products;
     private LocalDateTime createdAt;
